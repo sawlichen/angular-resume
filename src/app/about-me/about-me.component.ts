914d4d77aca0490f530d3aeb3545a8599c612270
LI CHEN SAW <li_chen_saw@dell.com>
2019-08-01 10:08:42 +0800

    changed message

---
+++ src/app/about-me/about-me.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-me',
+  templateUrl: './about-me.component.html',
+  styleUrls: ['./about-me.component.css']
+})
+export class AboutMeComponent implements OnInit {
+  navItemTitle = "About Me"
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule
